5f30fb1bcb082f9ed110b48bbc24eea1c365d4bf
matheus-orestes <100971661+matheus-orestes@users.noreply.github.com>
2023-01-30 16:59:18 -0300

    Add files via upload
    PS C:\Users\Matheus\Desktop\projetos do Matheus\projetos\PRO_1-1_C86_AtividadeDoAluno1-main>

---
+++ PRO_1-1_C86_AtividadeDoAluno1-main/PRO_1-1_C86_AtividadeDoAluno1-main/config.js
@@ -0,0 +1,10 @@
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDFUNfpUjVWdBr6Rar3lg4Dt-c-td7m0hk",
+  authDomain: "app-biblioteca-alimentar-57cd5.firebaseapp.com",
+  projectId: "app-biblioteca-alimentar-57cd5",
+  storageBucket: "app-biblioteca-alimentar-57cd5.appspot.com",
+  messagingSenderId: "411565100801",
+  appId: "1:411565100801:web:272fcdb3aaceae4ff59500",
+  measurementId: "G-9HNFS4NTWZ"
+};
---
+++ PRO_1-1_C86_AtividadeDoAluno1-main/PRO_1-1_C86_AtividadeDoAluno1-main/screens/CreateProduct.js
@@ -0,0 +1,194 @@
+import React, { Component } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  SafeAreaView,
+  Platform,
+  StatusBar,
+  ScrollView,
+  TextInput,
+  Dimensions,
+  FlatList,
+} from "react-native";
+import { RFValue } from "react-native-responsive-fontsize";
+import DropDownPicker from "react-native-dropdown-picker";
+
+import * as SplashScreen from 'expo-splash-screen';
+import * as Font from "expo-font";
+
+SplashScreen.preventAutoHideAsync();
+
+let customFonts = {
+  "Bubblegum-Sans": require("../assets/fonts/BubblegumSans-Regular.ttf"),
+};
+
+export default class CreateProduct extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      CreateProduct: '',
+      data: '',
+      fontsLoaded: false,
+      dropdownHeight: 40,
+    };
+  }
+
+  async _loadFontsAsync() {
+    await Font.loadAsync(customFonts);
+    this.setState({ fontsLoaded: true });
+  }
+
+  componentDidMount() {
+    this._loadFontsAsync();
+  }
+
+  render() {
+      return (
+        <View style={styles.container}>
+          <SafeAreaView style={styles.droidSafeArea} />
+          <View style={styles.appTitle}>
+            <View style={styles.appIcon}>
+            </View>
+            <View style={styles.appTitleTextContainer}>
+              <Text style={styles.appTitleText}>Novo Produto</Text>
+            </View>
+          </View>
+          <View style={styles.fieldsContainer}>
+            <View style={{ height: RFValue(this.state.dropdownHeight) }}>
+              <FlatList
+              renderItem={this.renderItem}
+                open={this.state.dropdownHeight == 170 ? true : false}
+                onOpen={() => {
+                  this.setState({ dropdownHeight: 170 });
+                }}
+                onClose={() => {
+                  this.setState({ dropdownHeight: 40 });
+                }}
+                style={{
+                  backgroundColor: "transparent",
+                  borderWidth: 1,
+                  borderColor: "white",
+                }}
+                textStyle={{
+                  color: this.state.dropdownHeight == 170 ? "black" : "white",
+                  fontFamily: "Bubblegum-Sans",
+                }}
+                onSelectItem={(item) =>
+                  this.setState({
+                    previewImage: item.value,
+                  })
+                }
+              />
+            </View>
+            <ScrollView>
+              <TextInput
+                style={styles.inputFont}
+                onChangeText={(title) => this.setState({ title })}
+                placeholder={"Products"}
+                placeholderTextColor="white"
+              />
+
+              <TextInput
+                style={[
+                  styles.inputFont,
+                  styles.inputFontExtra,
+                  styles.inputTextBig,
+                ]}
+                onChangeText={(description) => this.setState({ description })}
+                placeholder={"Description"}
+                multiline={true}
+                numberOfLines={4}
+                placeholderTextColor="white"
+              />
+              <TextInput
+                style={[
+                  styles.inputFont,
+                  styles.inputFontExtra,
+                  styles.inputTextBig,
+                ]}
+                onChangeText={(products) => this.setState({ products })}
+                placeholder={"Products"}
+                multiline={true}
+                numberOfLines={20}
+                placeholderTextColor="white"
+              />
+
+              <TextInput
+                style={[
+                  styles.inputFont,
+                  styles.inputFontExtra,
+                  styles.inputTextBig,
+                ]}
+               
+              />
+            </ScrollView>
+          </View>
+          <View style={{ flex: 0.08 }} />
+        </View>
+      );
+    }
+  }
+
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#15193c",
+  },
+  droidSafeArea: {
+    marginTop:
+      Platform.OS === "android" ? StatusBar.currentHeight : RFValue(35),
+  },
+  appTitle: {
+    flex: 0.07,
+    flexDirection: "row",
+  },
+  appIcon: {
+    flex: 0.3,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  iconImage: {
+    width: "100%",
+    height: "100%",
+    resizeMode: "contain",
+  },
+  appTitleTextContainer: {
+    flex: 0.7,
+    justifyContent: "center",
+  },
+  appTitleText: {
+    color: "white",
+    fontSize: RFValue(28),
+    fontFamily: "Bubblegum-Sans",
+  },
+  fieldsContainer: {
+    flex: 0.85,
+  },
+  previewImage: {
+    width: "93%",
+    height: RFValue(250),
+    alignSelf: "center",
+    borderRadius: RFValue(10),
+    marginVertical: RFValue(10),
+    resizeMode: "contain",
+  },
+  inputFont: {
+    height: RFValue(40),
+    marginTop: RFValue(40),
+    borderColor: "white",
+    borderWidth: RFValue(1),
+    borderRadius: RFValue(10),
+    paddingLeft: RFValue(10),
+    color: "white",
+    fontFamily: "Bubblegum-Sans",
+  },
+  inputFontExtra: {
+    marginTop: RFValue(15),
+  },
+  inputTextBig: {
+    textAlignVertical: "top",
+    padding: RFValue(5),
+  },
+});
